8ba8ef8a2280418d16662bd7d7a0cccd6bb566da
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2016-06-15 17:22:00 -0500

    Implemented the Auto Chooser dashboard and selection of Autonomous modes.

--- src/com/edinarobotics/follower/WaypointFollower.java
+++ src/com/edinarobotics/follower/WaypointFollower.java
@@ -1,17 +1,33 @@
 
 package com.edinarobotics.follower;
 
+import com.edinarobotics.follower.commands.AutonomousCommand;
+import com.edinarobotics.follower.commands.AutonomousCommand.AutonomousMode;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class WaypointFollower extends IterativeRobot {
 
+	private SendableChooser autoChooser;
+	
 	public void robotInit() {
-
+		Controls.getInstance();
+		Components.getInstance();
+		
+		setupDashboard();
 	}
 
 	public void autonomousInit() {
-
+		if (autoChooser == null) {
+			setupDashboard();
+		}
+		
+		Command command = new AutonomousCommand((AutonomousMode) autoChooser.getSelected());
+		command.start();
 	}
 
 	public void autonomousPeriodic() {
@@ -32,6 +48,15 @@ public class WaypointFollower extends IterativeRobot {
 
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
+	}
+	
+	public void setupDashboard() {
+		autoChooser = new SendableChooser();
+		
+		autoChooser.addDefault("Nothing", AutonomousMode.NOTHING);
+		autoChooser.addObject("Simple", AutonomousMode.SIMPLE);
+		
+		SmartDashboard.putData("Autonomous Chooser", autoChooser);
 	}
 
 }
