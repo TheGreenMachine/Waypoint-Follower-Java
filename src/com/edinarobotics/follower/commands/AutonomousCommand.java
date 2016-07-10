9f359b8c130bf965e1e905acb9dba65cb86960d7
ElliottOlson <elliotto17258@apps.edina.k12.mn.us>
2016-07-10 11:23:53 -0500

    Removed old Pathfinder-Java library and began work on implementing Team 254's Pathfinder library.

--- src/com/edinarobotics/follower/commands/AutonomousCommand.java
+++ src/com/edinarobotics/follower/commands/AutonomousCommand.java
@@ -1,8 +1,6 @@
 package com.edinarobotics.follower.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import jaci.pathfinder.Pathfinder;
-import jaci.pathfinder.Waypoint;
 
 public class AutonomousCommand extends CommandGroup {
 
@@ -11,10 +9,6 @@ public class AutonomousCommand extends CommandGroup {
 		switch (autoMode) {
 
 			case SIMPLE:
-	
-				addSequential(new DriveToWaypointCommand(new Waypoint(-4, -1, 0)));
-				addSequential(new DriveToWaypointCommand(new Waypoint(-2, -1, Pathfinder.d2r(-45))));
-				addSequential(new DriveToWaypointCommand(new Waypoint(-4, -1, Pathfinder.d2r(45))));
 	
 				break;
 	
